87a923b077c6b0b1fc1a5fc1838be433f1a3ae07
Deepika Mangnani <56153761+Deepika1520@users.noreply.github.com>
2021-02-18 00:39:27 +0530

    Create Day23Solution.py

---
+++ Day23/Day23Solution.py
@@ -0,0 +1,16 @@
+def Kclosest(self, arr, n, x, k):
+        max_heap = [ ( -1*abs(x-arr[i]) , -1*arr[i] ) for i in range(k) ]
+        heapq.heapify(max_heap)
+        # python heapq generates min heap
+        # it can be used as max_heap by multiplying elements by -1
+        
+        for i in range(k,n):
+            dist = -1*max_heap[0][0]
+            hno  = -1*max_heap[0][1]
+            if abs(arr[i]-x)<dist or ( abs(arr[i]-x)==dist and arr[i]<hno ):
+                heapq.heappop(max_heap)
+                heapq.heappush( max_heap, ( -1*abs(x-arr[i]) , -1*arr[i] ) )
+        
+        ret=[ -1*x[1] for x in max_heap]
+        ret.sort()
+        return ret
